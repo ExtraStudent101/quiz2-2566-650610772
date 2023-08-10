2ef1d956f394696845ce4bfca7a1142fa7f9f5e7
ExtraStudent101 <theerapat_lum@cmu.ac.th>
2023-08-10 15:37:39 +0700

    GG

--- src/components/PostOwner.jsx
+++ src/components/PostOwner.jsx
@@ -1,3 +1,26 @@
-export const PostOwner = ({}) => {
-  return <div>Your code here...</div>;
+export const PostOwner = (Prop) => {
+  return (
+    <div className="vstack gap-3">
+      <div className="d-flex align-items-center gap-3">
+        <img
+          src="/profileImages/IMG_0163.jpg"
+          width="48"
+          height="48"
+          className="rounded-circle"
+          style={{ objectFit: "cover" }}
+        />
+        <span className="fw-semibold fs-5">
+          {Prop.name} {Prop.id}
+        </span>
+      </div>
+
+      <span>{Prop.quote}</span>
+
+      <div className="d-flex align-items-center gap-1">
+        <img src="/like.svg" width={20}></img>
+        <span className="text-muted">100 คน</span>
+      </div>
+      <hr className="m-0 border" />
+    </div>
+  );
 };
